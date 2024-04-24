8ad11d31518f6226caa681eacd815e8e7152e5e7
Mohammed <dev.mohammedsh@gmail.com>
2024-04-24 12:35:40 +0530

    Activity 9

--- src/bin/a9.rs
+++ src/bin/a9.rs
@@ -9,4 +9,21 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+// * Use a function that returns a tuple
+fn coordinate() -> (i32, i32) {
+    (3, 7)
+}
+
+fn main() {
+    // * Destructure the return value into two variables
+    let (_x, y) = coordinate();
+
+    // * Use an if..else if..else block to determine what to print
+    if y > 5 {
+        println!(">5");
+    }else if y < 5 {
+        println!("<5");
+    }else {
+        println!("==5");
+    }
+}
