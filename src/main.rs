2f0b4dac18ef6ffe0e83c7b520cfbb6ad5eb5477
Mohammed <dev.mohammedsh@gmail.com>
2024-04-25 16:53:22 +0530

    new

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// This is the entry point of the 
+// application.
 fn main() {
+    // Display a message to the user
     println!("Hello, world!");
+
+    // my favorite color
+    let c = "black"; // avoid
+
+    let my_favorite_color = "black";
 }
